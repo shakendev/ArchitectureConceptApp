a9d1860b199aab4e2c0e85754baaa1023376586c
Dmitry Novikov <nvkv.dmitry@gmail.com>
2025-04-25 12:48:38 +0300

    refactor: post composer improvements

--- ConceptApp/ConceptApp/Features/Post/Sources/Post/Composition/PostComposer.swift
+++ ConceptApp/ConceptApp/Features/Post/Sources/Post/Composition/PostComposer.swift
@@ -6,28 +6,23 @@ import Comments
 @MainActor
 public struct PostComposer<Resolver: MLNResolvableContainer> {
     private let resolver: Resolver
-    private let deps: PostDependencies
 
     public init(resolver: Resolver) {
         self.resolver = resolver
-        deps = .init(resolver: resolver)
     }
 
     public func compose(context: Context) -> Feature {
+        let deps = PostDependencies(resolver: resolver)
         let viewModel = PostViewModel(id: context.id, fetcher: deps.fetcher)
-        let commentsScreen = composeCommentsScreen(id: context.id)
-        let screen = PostScreen(
-            comments: commentsScreen,
-            viewModel: viewModel,
-            events: context.events
-        )
+        let commentsScreen = composeCommentsScreen(id: context.id, event: context.events.onHapticFeedback)
+        let screen = PostScreen(comments: commentsScreen, viewModel: viewModel, events: context.events)
 
         return .init(screen: screen)
     }
 
-    private func composeCommentsScreen(id: Int) -> AnyView {
+    private func composeCommentsScreen(id: Int, event: @escaping PostEvents.FeedbackAction) -> AnyView {
         let composer = CommentsComposer(resolver: resolver)
-        let events = CommentsEvents(onHapticFeedback: deps.feedback.generate)
+        let events = CommentsEvents(onHapticFeedback: event)
         let feature = composer.compose(context: .init(id: id, events: events))
 
         return feature.screen
