d4aa21282787364fb3a0f7d2e8db894a30847bb3
Dmitry Novikov <nvkv.dmitry@gmail.com>
2025-05-10 13:55:13 +0300

    refactor: remove extra comments

--- ConceptApp/ConceptApp/Core/Core/Sources/Core/Services/NetworkConfig.swift
+++ ConceptApp/ConceptApp/Core/Core/Sources/Core/Services/NetworkConfig.swift
@@ -7,8 +7,6 @@ import MelonKit
 // MARK: - NetworkConfig
 
 public final class NetworkConfig: MLNNetworkConfiguration {
-    // This line is changed by a script for each of the existing project schemes, except for the Periphery scheme
-    // See more in Config Shell Scripts folder
     public override var name: StaticString { "dummyjson" }
     public override var domain: StaticString { "com" }
 }
